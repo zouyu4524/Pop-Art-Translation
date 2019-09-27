cc147c3bd7d01fcd902e2c44d095f17f47f2fccc
zouyu4524 <920789718@qq.com>
2019-09-27 15:53:58 +0800

    load three groups of results at once

--- load_results.py
+++ load_results.py
@@ -1,25 +1,43 @@
 import pickle
 import matplotlib.pyplot as plt
 
-
-with open('SGD-only-whole-set.pkl', 'rb') as f:
+with open('SGD.pkl', 'rb') as f:
     samples = pickle.load(f)
     m = pickle.load(f)
     l = pickle.load(f)
     u = pickle.load(f)
 
-with open('Art-only-whole-set.pkl', 'rb') as f:
+with open('ART-3.5.pkl', 'rb') as f:
     samples_ = pickle.load(f)
     m_ = pickle.load(f)
     l_ = pickle.load(f)
     u_ = pickle.load(f)
 
+with open('PopArt.pkl', 'rb') as f:
+    samples__ = pickle.load(f)
+    m__ = pickle.load(f)
+    l__ = pickle.load(f)
+    u__ = pickle.load(f)
+
 
 fig = plt.figure()
 spl = fig.add_subplot(111)
-spl.plot(samples, m, color='C0')
-spl.fill_between(samples, u, l, facecolor='C0', alpha=0.5)
-spl.plot(samples_, m_, color='C1')
-spl.fill_between(samples_, u_, l_, facecolor='C1', alpha=0.5)
+
+spl.plot(samples__, m__, color='C2', label='PopArt')
+spl.fill_between(samples__, u__, l__, facecolor='C2', alpha=0.5)
+
+spl.plot(samples_, m_, color='C0', label='ART')
+spl.fill_between(samples_, u_, l_, facecolor='C0', alpha=0.5)
+
+spl.plot(samples, m, color='C3', label='SGD')
+spl.fill_between(samples, u, l, facecolor='C3', alpha=0.5)
+
 spl.set_yscale("log")
+
+plt.xlabel('# samples')
+plt.ylabel('RMSE (log scale)')
+plt.xlim((0, 5000))
+
+spl.legend(loc="lower left", frameon=False)
+
 plt.show()
